5862e02de896689035f535cc3c1ea4c84191b0aa
alik-r <alikamran.rza@gmail.com>
2023-07-26 11:07:14 +0400

    Add keyboard support

--- script.js
+++ script.js
@@ -68,15 +68,13 @@ function evalPostfix(postfixArr) {
     return stack[0];
 }
 
-const btnEquals = document.querySelector('.btn.equals');
-btnEquals.addEventListener('click', () => {
-    // console.log('Pressed =');
-    // console.log('Display:', divDisplay.textContent);
-    // console.log('RPN:', infixToPostfix(divDisplay.textContent));
-    // console.log('Results:', evalPostfix(infixToPostfix(divDisplay.textContent)));
-    divDisplay.textContent = evalPostfix(infixToPostfix(divDisplay.textContent));
-});
 
+function calculateResult() {
+  divDisplay.textContent = evalPostfix(infixToPostfix(divDisplay.textContent));
+}
+
+const btnEquals = document.querySelector('.btn.equals');
+btnEquals.addEventListener('click', calculateResult);
 
 // Numbers
 
@@ -105,15 +103,15 @@ const btnOp = document.querySelectorAll('.btn.operator');
 btnOp.forEach(btn => btn.addEventListener('click', displayAddOp));
 
 // Clear
-const btnClear = document.querySelector('.btn.clear');
 
 function clearDisplay() {
     divDisplay.textContent = '0';
 }
 
+const btnClear = document.querySelector('.btn.clear');
 btnClear.addEventListener('click', clearDisplay);
 
-// Backspace button
+// Backspace 
 const btnBackspace = document.querySelector('.btn.backspace');
 
 function pressBackspace() {
@@ -123,3 +121,46 @@ function pressBackspace() {
 }
 
 btnBackspace.addEventListener('click', pressBackspace);
+
+// Keyboard support
+
+document.addEventListener('keydown', (event) => {
+  const key = event.key;
+  if (key === 'Enter') {
+    // Press 'Enter' key for '=' operation
+    event.preventDefault(); 
+    calculateResult();
+  } else if (key === 'Escape') {
+    // Press 'Escape' key for 'C' (Clear) operation
+    clearDisplay();
+  } else if (key === 'Backspace') {
+    // Press 'Backspace' key for '⌫' (Backspace) operation
+    pressBackspace();
+  } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+    displayAddOpKeyb(key);
+  } else if (/^[0-9.]$/.test(key)) {
+    displayAddNumberKeyb(key);
+  }
+});
+
+function displayAddNumberKeyb(key) {
+  const num = key;
+  if (divDisplay.textContent === '0') {
+    divDisplay.textContent = num;
+  } else {
+    divDisplay.textContent += num;
+  }
+}
+
+function displayAddOpKeyb(key) {
+  const op = key;
+  if (!['+', '-', '*', '/'].includes(divDisplay.textContent.at(-1))) {
+    divDisplay.textContent += op;
+  }
+}
+
+function pressBackspace() {
+  if (divDisplay.textContent.length > 1) {
+    divDisplay.textContent = divDisplay.textContent.slice(0, -1);
+  }
+}
